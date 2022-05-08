5ab56530852c25d3c803b78439795bad5047f2b1
Pavel <pashatrofimov@mail.ru>
2022-05-08 09:50:18 +0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int cost = 3000; // стоимость билета
+        int bonus = 20; // количество рублей для одной мили
+        int miles = cost / bonus;
+
+        System.out.println(miles);
+    }
+}
